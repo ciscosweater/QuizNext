19691bc8965aa2e6e78a0c7ed1817d88dc911a67
Gustavo Francisco <ciscosweater@gmail.com>
2023-06-19 15:29:12 -0300

    Componente de respostas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
 import Questao from '@/components/Questao'
 import QuestaoModel from '@/model/questao'
 import RespostaModel from '@/model/resposta'
+import { useState } from 'react'
+
+const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
+  RespostaModel.errada('Verde'),
+  RespostaModel.errada('Vermelha'),
+  RespostaModel.errada('Azul'),
+  RespostaModel.certa('Preta'),
+])
 
 export default function Home() {
-  const questaoTeste = new QuestaoModel(1, 'Melhor cor?', [
-    RespostaModel.errada('Verde'),
-    RespostaModel.errada('Vermelha'),
-    RespostaModel.errada('Azul'),
-    RespostaModel.certa('Preta'),
-  ])
-  
+  const [questao, setQuestao] = useState(questaoMock)
+
+  function respostaFornecida(indice: number) {
+    setQuestao(questao.responderCom(indice))
+  }
+
   return (
-    <div style={{display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center'}}>
-      <Questao valor={questaoTeste} />
+    <div style={{ display: 'flex', height: '100vh', justifyContent: 'center', alignItems: 'center' }}>
+      <Questao valor={questao} respostaFornecida={respostaFornecida} />
     </div>
   )
 }
