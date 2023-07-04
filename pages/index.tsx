880eb92695f4c17bc99ced1cf75cec5fec4d29ba
Gustavo Francisco <ciscosweater@gmail.com>
2023-07-04 19:19:28 -0300

    Integração com a API do próprio Next para carregar as questões

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,70 @@
-import Botao from '@/components/Botao'
-import Questao from '@/components/Questao'
+import Questionario from '@/components/Questionario'
 import QuestaoModel from '@/model/questao'
-import RespostaModel from '@/model/resposta'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
 
-const questaoMock = new QuestaoModel(1, 'Qual é a melhor cor?', [
-  RespostaModel.errada('Verde'),
-  RespostaModel.errada('Vermelha'),
-  RespostaModel.errada('Azul'),
-  RespostaModel.certa('Preta'),
-])
+const BASE_URL = 'http://localhost:3000/api';
 
 export default function Home() {
-  const [questao, setQuestao] = useState(questaoMock)
+  const router = useRouter();
 
-  function respostaFornecida(indice: number) {
-    setQuestao(questao.responderCom(indice));
-  };
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<any>()
+  const [pontos, setPontos] = useState<number>(0)
 
-  function tempoEsgotado() {
-    if (questao.naoRespondida) {
-      setQuestao(questao.responderCom(-1));
-    }
-  };
+  async function carregarIdsDasQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
 
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh', justifyContent: 'center', alignItems: 'center' }}>
-      <Questao valor={questao} respostaFornecida={respostaFornecida} tempoEsgotado={tempoEsgotado} tempoPraResposta={5} />
-      <Botao texto="Próxima" href="/resultado" />
-    </div>
-  )
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const json = await resp.json()
+    const novaQuestao = QuestaoModel.criarUsandoObjeto(json)
+    setQuestao(novaQuestao)
+  }
+
+  useEffect(() => {
+    carregarIdsDasQuestoes()
+  }, [])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes])
+
+  function irPraProximoPasso() {
+    const proximoId = idProximaPergunta()
+    proximoId ? irPraProximaQuestao(proximoId) : finalizar()
+  }
+
+  function idProximaPergunta() {
+    const proximoIndice = idsDasQuestoes.indexOf(questao.id) + 1
+    return idsDasQuestoes[proximoIndice]
+  }
+
+  function questaoRespondida(questaoRespondida: QuestaoModel) {
+    setQuestao(questaoRespondida)
+    const acertou = questaoRespondida.acertou
+    setPontos(pontos + (acertou? 1 : 0))
+  }
+
+  function irPraProximaQuestao(proximoId: number) {
+    carregarQuestao(proximoId)
+  }
+
+  function finalizar() {
+    router.push({pathname: '/resultado', query: {
+      total: idsDasQuestoes.length,
+      certas: pontos
+    }})
+  }
+
+  return questao?
+    (<Questionario 
+      questao={questao}
+      ultima={idProximaPergunta() === undefined}
+      questaoRespondida={questaoRespondida}
+      irPraProximoPasso={irPraProximoPasso}
+    />)  :  false
 }
--- pages/resultado.tsx
+++ pages/resultado.tsx
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 export default function Resultado() {
+    const router = useRouter();
+    const total: any = router.query.total;
+    const pontos: any = router.query.certas;
+    const percentual = Math.round((pontos / total) * 100)
     return (
         <div>
             <h1>Resultado</h1>
+            <div>{total}</div>
+            <div>{pontos}</div>
+            <div>{`${percentual}%`}</div>
         </div>
     )
 }
